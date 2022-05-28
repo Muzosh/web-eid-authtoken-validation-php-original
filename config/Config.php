1a949b8b3630b30dedb9d207387569a964d80eb8
Muzosh <30979983+Muzosh@users.noreply.github.com>
2022-05-28 22:47:22 +0300

    finish readme and changelog

--- config/Config.php
+++ config/Config.php
@@ -7,6 +7,5 @@ $root_diretory = dirname($current_directory);
 // Config
 define('CONFIG', array(
     'log_file_path' => $root_diretory.'/web-eid-authtoken-validation-php.log',
-    'trusted_certs_path' => $current_directory.'/trustedcerts',
 	'nonce_length' => 32
 ));
